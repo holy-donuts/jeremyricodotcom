3da584087df67e6db90480e2c3ca941557d482e6
Jeremy Rico <jeremy.rico35@gmail.com>
2023-02-14 20:15:42 -0800

    added navbar home page and projects

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from projects.models import Project
+
+class Projects(admin.ModelAdmin):
+    pass
+
+admin.site.register(Project, Projects)
 
 # Register your models here.
